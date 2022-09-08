860e057944c7a24064b4e4bde3d872d1b2b6964b
Archit Rathod <75872913+Archit1706@users.noreply.github.com>
2022-09-08 19:44:26 +0530

    Update Multiplication.java

--- Multiplication.java
+++ Multiplication.java
@@ -1,5 +1,5 @@
 public class Multiplication {
-  public static void main() {
+  public static void main(String args[]) {
     int n = 88;
     for (int i = 1; i <= 10; i++) {
       System.out.println(n * i);
